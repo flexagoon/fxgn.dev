9f1c1ae6fd1162256d4124a649029f909f338244
flexagoon <contact@flexagoon.com>
2023-01-17 19:57:01 +0300

    rewrite the website to SvelteKit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,18 @@
+<a href="/" class="button">
+	<img src="/images/logo-white.svg" alt="logo" />
+</a>
+
+<style lang="scss">
+	$logo-size: 80px;
+
+	a {
+		margin-left: auto;
+		margin-right: auto;
+		display: block;
+		width: $logo-size;
+	}
+
+	img {
+		width: $logo-size;
+	}
+</style>
---
+++ src/routes/links/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Links</h1>
+<a href="https://github.com/flexagoon" class="button">GitHub</a>
+<a href="https://t.me/flexagoon" class="button">Telegram</a>
+<a href="https://reddit.com/u/sevenisnotanumber2" class="button">Reddit</a>
+<a href="https://youtube.com/channel/UCT_zuaWs6SitJF2lvuHO7xw" class="button">YouTube</a>
+<a href="mailto:contact@flexagoon.com" class="button">E-Mail</a>
+<br />
+<br />
+<a href="/" class="button">Back</a>

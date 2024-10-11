9095966526ee47f1f4013c79076ac325c00cee85
flexagoon <code@fxgn.dev>
2024-10-11 18:11:07 +0300

    don't include content in rss because mdx

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,5 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import sanitizeHtml from "sanitize-html";
-import MarkdownIt from "markdown-it";
-const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("blog");
@@ -14,9 +11,6 @@ export async function GET(context) {
     items: posts.map((post) => ({
       ...post.data,
       link: `/blog/${post.slug}/`,
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
-      }),
     })),
   });
 }
